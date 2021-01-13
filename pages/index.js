0bae59d65734a19ad69926bb0ce7028d847ab1fd
rojrun <runroj@gmail.com>
2021-01-12 19:38:50 -0800

    layout for microphone component done

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Card, EmptyState, Layout, Page, Button, ButtonGroup, DisplayText, Link} from '@shopify/polaris';
 import {ResourcePicker, TitleBar} from '@shopify/app-bridge-react';
 import store from 'store-js';
-// import ResourceListWithProducts from '../components/ResourceList';
 import {Redirect} from '@shopify/app-bridge/actions';
 import {Context} from '@shopify/app-bridge-react';
+
 
 // const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 const img = '../images/AVbyFPLogo_Horizontal_360x.png';
@@ -14,7 +14,7 @@ class Index extends React.Component {
 
   render() {
     const app = this.context;
-
+    
     const redirectToHeadphones = () => {
       const redirect = Redirect.create(app);
       redirect.dispatch(
@@ -35,12 +35,12 @@ class Index extends React.Component {
       <Page fullWidth title="Product Search">
         <Layout>
           <Layout.Section>
-            <Card title="Headphones" sectioned>
+            <Card sectioned>
               <Link onClick={() => {redirectToHeadphones();}}>Headphones</Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
-            <Card title="Microphones" sectioned>
+            <Card sectioned>
               <Link onClick={() => {redirectToMicrophones();}}>Microphones</Link>
             </Card>
           </Layout.Section>
@@ -93,7 +93,7 @@ class Index extends React.Component {
   
 //   handleSelection = (resources) => {
 //     const idsFromResources = resources.selection.map((product) => product.id);
-//     this.setState({open: false})
+//     this.setState({open: false});
 //     store.set('ids', idsFromResources);
 //   };
 }
--- pages/microphones.js
+++ pages/microphones.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
-import {Card, ResourceList, Stack, TextStyle, Thumbnail, Layout, MediaCard, Page} from '@shopify/polaris';
+import {Card, ResourceList, ResourceItem, Stack, TextStyle, Thumbnail, Layout, MediaCard, Page, Avatar} from '@shopify/polaris';
 import store from 'store-js';
 import {Redirect} from '@shopify/app-bridge/actions';
 import {Context} from '@shopify/app-bridge-react';
 import GET_PRODUCTS_BY_PRODUCT_TYPE from '../graphQL/getProductsByProductType.js';
+import ProductResults from '../components/ProductResults.js';
 
 class Microphones extends React.Component {
   static contextType = Context;
+  constructor(props) {
+    super(props);
+  }
 
   render() { 
     return (
@@ -18,79 +22,39 @@ class Microphones extends React.Component {
               if (loading) return <div>Loading...</div>;
               if (error) return <div>{error.message}</div>;
               
-              const productResults = data.products.edges;
-              console.log("productResults: ", productResults);
-              productResults.map(product => {
-                console.log("single product: ", product);
-                const productVendor = product.node.vendor;
-                console.log("productVendor: ", productVendor);
-
-                var prodATCategory = {}, 
-                    prodNeumannCategory = {},
-                    prodSennheiserCategory = {}, 
-                    prodShureCategory = {};    
-
-                switch (productVendor) {
-                  case 'Audio-Technica':
-                    prodATCategory = product;
-                    // audioTechnica.push(product);
-                    console.log("AT: ", prodATCategory.node.title);
-                    break;
-                  case 'Neumann':
-                    prodNeumannCategory = product;
-                    // neumann.push(product);
-                    // console.log("neumann: ", neumann);
-                    break;
-                  case 'Sennheiser':
-                    prodSennheiserCategory = product;
-                    // sennheiser.push(product);
-                    // console.log("sennheiser: ", sennheiser);
-                    break;
-                  case 'Shure':
-                    prodShureCategory = product;
-                    // shure.push(product);
-                    // console.log('shure: ', shure);
-                    break;
-                }
-                console.log("AT: ", prodATCategory.node.title);
+              const productResults = data.products.edges;       
                 return (
                   <Layout>
                     <Layout.Section primary>
                       <Card title="Results">
-                        <Card.Section title="Audio-Technica">
-                          <MediaCard
-                            title={prodATCategory.node.title}
-                            description={prodATCategory.node.handle}
-                            size="small"
-                          >
-                            <img
-                              alt={prodATCategory.node.images.edges[0].node.altText || ""}
-                              width="100%"
-                              height="100%"
-                              style={{
-                                objectFit: 'cover',
-                                objectPosition: 'center'
-                              }}
-                              src={prodATCategory.node.images.edges[0].node.originalSrc}
-                            />
-                          </MediaCard>
-                        </Card.Section>  
-                        <Card.Section title="Neumann">
-                          <Card>
-                            {prodNeumannCategory}
-                          </Card>
-                        </Card.Section> 
-                        <Card.Section title="Sennheiser">
-                          <Card>
-                            {prodSennheiserCategory}
-                          </Card>
-                        </Card.Section>
-                        <Card.Section title="Shure">
-                          <Card>
-                            {prodShureCategory}
-                          </Card>
-                        </Card.Section>            
+                        <Card.Section title="Vendors">
+                          <ResourceList
+                            resourceName={{singular: 'product', plural: 'products'}}
+                            items={productResults}
+                            renderItem={(item) => {
+                              const media = <Thumbnail
+                                source={item.node.images.edges[0].node.originalSrc}
+                                size="small"
+                                alt={item.node.images.edges[0].node.altText}
+                              />  
+                              return (
+                                <ResourceItem
+                                  id={item.node.id}
+                                  media={media}  
+                                >
+                                  <h3>
+                                    <TextStyle variation="strong">{item.node.title}</TextStyle>
+                                  </h3>
+                                  <p>${item.node.variants.edges[0].node.price}</p>
+                                </ResourceItem>
+                              );
+                            }}
+                            showHeader
+                            totalItemsCount={productResults.length}
+                          />  
+                        </Card.Section>      
                       </Card>
+                      {/* <ProductResults id={product.node.id} image={product.node.images} title={product.node.title} variants={product.node.variants} vendor={product.node.vendor}/> */}
                     </Layout.Section>
                     <Layout.Section secondary>
                       <Card title="Filters">
@@ -106,68 +70,11 @@ class Microphones extends React.Component {
                     </Layout.Section>
                   </Layout>  
                 );            
-              });
+              // });
             }}
         </Query>
-      </Page>
+      </Page>    
     );  
   };
 }
 export default Microphones;
-
- {/* <Query query={GET_PRODUCTS_BY_PRODUCT_TYPE} variables={{product_type: "microphone"}}>
-          {({data, loading, error}) => {
-            if (loading) return <div>Loading...</div>;
-            if (error) return <div>{error.message}</div>;
-            console.log('data ', data.products.edges);
-                     
-            return (
-              <Card>
-                <ResourceList
-                  showHeader
-                  resourceName={{singular: 'Product', plural: 'Products'}}
-                  items={data.products.edges}
-                  renderItem={item => {
-                    console.log("item ", item);
-                    const media = (
-                      <Thumbnail
-                        source={
-                          item.node.images.edges[0]
-                            ? item.node.images.edges[0].node.originalSrc
-                            : ''
-                        }
-                        alt={
-                          item.node.images.edges[0]
-                          ? item.node.images.edges[0].node.altText
-                          : ''
-                        }
-                      />
-                    );
-                    const price = item.node.variants.edges[0].node.price;
-
-                    return (
-                      <ResourceList.Item
-                        id={item.node.id}
-                        media={media}
-                        accessibilityLabel={`View details for ${item.node.title}`}            
-                      >
-                        <Stack>
-                          <Stack.Item>
-                            <h3>
-                              <TextStyle variation="strong">
-                                {item.node.title}
-                              </TextStyle>
-                            </h3>
-                          </Stack.Item>
-                          <Stack.Item>
-                            <p>${price}</p>
-                          </Stack.Item>
-                        </Stack>
-                      </ResourceList.Item>
-                    );
-                  }}
-                />
-              </Card>
-            );
-          }}
-        </Query> */}
