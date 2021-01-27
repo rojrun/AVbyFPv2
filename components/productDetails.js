51fe069669dff5b5887bf9e47853a22a66aaf35e
rojrun <runroj@gmail.com>
2021-01-26 19:00:00 -0800

    added state to parent component

--- components/productDetails.js
+++ components/productDetails.js
@@ -5,50 +5,57 @@ import {Button, ButtonGroup, Card, Layout, Stack} from '@shopify/polaris';
 class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
+   
+    // console.log("object props: ", (Object.keys(this.props.productsList)[0]).values(this.props.productsList)[0]);
   }
 
   render() {
-    console.log("productDetails props: ", this.props.product);
-    console.log("handleProductDetails: ", this.props.handleProductDetails);
+    // console.log("productsList: ", this.props.productsList);
+    // console.log("key: ", Object.keys(this.props.productsList)[0]);
+    // console.log("value: ", (this.props.productsList[Object.keys(this.props.productsList)[0]])[0]);
+
+  //   console.log("productToDisplay props: ", this.props.productToDisplay);
+    
     return (
-      <Layout.Section primary>
-        <Card title={this.props.product.node.title}>
-          <Card.Section title="Images">
-            {this.props.product.node.hasOnlyDefaultVariant
-              ? <div></div>
-              : this.props.product.node.variants.edges.map((variant) => {
+      <h1>hi</h1>
+  //     <Layout.Section primary>
+  //       <Card title={this.props.product.node.title}>
+  //         <Card.Section title="Images">
+  //           {this.props.product.node.hasOnlyDefaultVariant
+  //             ? <div></div>
+  //             : this.props.product.node.variants.edges.map((variant) => {
                 
-                return (
-                  <div>{variant.node.title}</div>
-                )
-              })
-            }
-          </Card.Section>
-          <Card.Section title="Description">
-            <div>
-              {this.props.product.node.descriptionHtml.indexOf('</') !== -1
-                ? (
-                  <div dangerouslySetInnerHTML={{__html: this.props.product.node.descriptionHtml.replace(/(<? *script)/gi, 'illegalscript')}}>
-                  </div>
-                  )
-                : this.props.product.node.descriptionHtml  
-              }
-            </div>
-          </Card.Section>
-          <Card.Section>
-            <Stack distribution="trailing">
-              <ButtonGroup>
-                <p>${this.props.product.node.variants.edges[0].node.price}</p>
-                <div style={{color: '#000000'}}>
-                  <Button monochrome outline>
-                    Add to Cart
-                  </Button>
-                </div>
-              </ButtonGroup>
-            </Stack>
-          </Card.Section>   
-        </Card>
-      </Layout.Section>
+  //               return (
+  //                 <div>{variant.node.title}</div>
+  //               )
+  //             })
+  //           }
+  //         </Card.Section>
+  //         <Card.Section title="Description">
+  //           <div>
+  //             {this.props.product.node.descriptionHtml.indexOf('</') !== -1
+  //               ? (
+  //                 <div dangerouslySetInnerHTML={{__html: this.props.product.node.descriptionHtml.replace(/(<? *script)/gi, 'illegalscript')}}>
+  //                 </div>
+  //                 )
+  //               : this.props.product.node.descriptionHtml  
+  //             }
+  //           </div>
+  //         </Card.Section>
+  //         <Card.Section>
+  //           <Stack distribution="trailing">
+  //             <ButtonGroup>
+  //               <p>${this.props.product.node.variants.edges[0].node.price}</p>
+  //               <div style={{color: '#000000'}}>
+  //                 <Button monochrome outline>
+  //                   Add to Cart
+  //                 </Button>
+  //               </div>
+  //             </ButtonGroup>
+  //           </Stack>
+  //         </Card.Section>   
+  //       </Card>
+  //     </Layout.Section>
     );
   }
 }
--- pages/products.js
+++ pages/products.js
@@ -7,50 +7,50 @@ import ProductResults from '../components/productResults.js';
 import ProductDetails from '../components/productDetails.js';
 
 class Products extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      productsList: {},
-      productToDisplay: {}
+  state = {
+    isFirstRender: true,
+    productsList: {},
+    productToDisplay: (this.state.productsList[Object.keys(this.props.productsList)[0]])[0]
+  }
+
+  restructureData = data => {
+     // Searches for vendor to use as keys in productsList
+     const vendorKeys = data.products.edges.reduce((allProducts, current) => {
+      return allProducts.includes(current.node.vendor) ? allProducts : allProducts.concat([current.node.vendor]);
+    }, []);
+    vendorKeys.sort();
+    
+    // Makes products array for object, with vendor as keys
+    const productsList = {};
+    vendorKeys.map((vendor) => {
+      const products = data.products.edges.filter((product) => product.node.vendor === vendor);
+      productsList[vendor] = products;
+    });
+    
+    if (this.state.isFirstRender) {
+      this.setState({productsList, isFirstRender: false});
     }
   }
-  
+
   render() {
-    const productTypeVar = store.get('productType');
+    const productType = store.get('productType');
     const productTypeObj = {};
-    productTypeObj.product_type = productTypeVar;
+    productTypeObj.product_type = `product_type:${productType} status:ACTIVE inventory_total:>0`;
     
     return (
-      <Page title={`${(productTypeVar + 's').toUpperCase()}`} fullWidth> 
-        <Query query={GET_PRODUCTS_BY_PRODUCT_TYPE} variables={productTypeObj}>
-          {({data, loading, error}) => {
-            if (loading) return <div>Loading...</div>;
-            if (error) return <div>{error.message}</div>;
-
-            // Filter out product with no inventory
-            const filteredProductResults = (data.products.edges).filter((product) => product.node.totalInventory > 0);
-            
-            // Searches for vendor to use as keys in productsList
-            const vendorKeys = filteredProductResults.reduce((allProducts, current) => {
-              return allProducts.includes(current.node.vendor) ? allProducts : allProducts.concat([current.node.vendor]);
-            }, []);
-            vendorKeys.sort();
-            
-            // Makes products array for object, with vendor as keys
-            vendorKeys.map((vendor) => {
-              const products = filteredProductResults.filter((product) => product.node.vendor === vendor);
-              this.setState({
-                productsList: this.state.productsList[vendor] = products
-              });
-            });
-            
-            return (
-              <ProductResults productsList={this.state.productsList} filteredProductResults={filteredProductResults}/>
-            );    
-          }}
-        </Query>
-        {/* <ProductDetails /> */}
-      </Page>    
+      <Query query={GET_PRODUCTS_BY_PRODUCT_TYPE} variables={productTypeObj} onCompleted={data => this.restructureData(data)}>
+        {({data, loading, error}) => {
+          if (loading) return <div>Loading...</div>;
+          if (error) return <div>{error.message}</div>;
+          
+          return (
+            <Page title={`${(productType + 's').toUpperCase()}`} fullWidth> 
+              <ProductResults productsList={this.state.productsList} originalDataListCount={data.products.edges.length}/>
+              <ProductDetails productsList={this.state.productsList} productToDisplay={this.state.productToDisplay}/>
+            </Page>
+          );    
+        }}
+      </Query>
     );  
   }
 }    
