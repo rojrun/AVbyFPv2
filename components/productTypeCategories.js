bcf8ebdfe6cc1fff494906f54db87ed4224d9d4c
rojrun <runroj@gmail.com>
2021-02-06 19:38:17 -0800

    made link components for index page

--- components/productTypeCategories.js
+++ components/productTypeCategories.js
@@ -1,42 +1,48 @@
 import React from 'react';
 import {Card, Layout, Link} from '@shopify/polaris';
 import store from 'store-js';
-import {Redirect} from 'next/dist/next-server/server/api-utils';
+import {Redirect} from '@shopify/app-bridge/actions';
 import {Context} from '@shopify/app-bridge-react';
 
 // Child component for index.js. Display menu links in home page.
 class ProductTypeCategories extends React.Component {
+  static contextType = Context;
   constructor(props) {
     super(props);
-    console.log("ProductTypeCategories props: ", this.props);
+    this.state = {
+      app: {}
+    }
   }
 
-  render() {  
-    return (
-      <Layout.section>
-        <Card>
-          <Link>
-            {this.props.productType}
-          </Link>
-        </Card>
-      </Layout.section>
-    );
-    // return (
-    //   <Layout.Section>
-    //     <Card sectioned>
-    //       <Link
-    //         onClick={() => {
-    //           console.log("link clicked!");
-    //           store.remove('productType');
-    //           store.set('productType', this.props.productType);
-    //           // this.props.handleRedirect();
-    //         }}
-    //       >
-    //         {`${(this.props.productType +'s').toUpperCase()}`}  
-    //       </Link>
-    //     </Card>
-    //   </Layout.Section>
-    // );
+  componentDidMount() {
+    this.setState({app: this.context});
   }
+
+  redirectToProducts = () => {
+    store.remove('productType');
+    store.set('productType', this.props.productType);
+    const redirect = Redirect.create(this.state.app);
+    redirect.dispatch(
+      Redirect.Action.APP,
+      '/products',
+    );   
+  };
+
+  capitalizeFirstLetter = (str) => {
+    str += 's';
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  render() { 
+    return (
+        <Layout.Section>
+          <Card sectioned>
+            <Link onClick={() => {this.redirectToProducts()}}>
+              {this.capitalizeFirstLetter(this.props.productType)}  
+            </Link>
+          </Card>
+        </Layout.Section>
+      );
+    }
 }
 export default ProductTypeCategories;
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,16 @@
 import React from 'react';
-import {Card, EmptyState, Layout, Page, Button, ButtonGroup, DisplayText, Link} from '@shopify/polaris';
-import {Query} from 'react-apollo';
-import {ResourcePicker, TitleBar} from '@shopify/app-bridge-react';
-import store from 'store-js';
-import {Redirect} from '@shopify/app-bridge/actions';
-import {Context} from '@shopify/app-bridge-react';
+import {Layout, Page} from '@shopify/polaris';
 import ProductTypeCategories from '../components/productTypeCategories.js';
 
-class Index extends React.Component {
-  static contextType = Context;
- 
-  // redirectToProducts = () => {
-  //   const app = this.context;
-  //   const redirect = Redirect.create(app);
-  //   redirect.dispatch(
-  //     Redirect.Action.APP,
-  //     '/products',
-  //   );   
-  // };
-  
-  render() {
-    const app = this.context;
-    const redirect = Redirect.create(app);
-    redirect.dispatch(
-      Redirect.Action.APP,
-      '/products',
-    );   
-    
-    return (
-      <Page fullWidth title="Product Search">
-        <Layout>
-          <ProductTypeCategories productType="audio mixer" />
-          <ProductTypeCategories productType="case" />
-          <ProductTypeCategories productType="headphone" />
-          <ProductTypeCategories productType="microphone" />
-          {/* <Layout.Section>
-            <Card sectioned>
-              <Link 
-                onClick={() => {
-                  store.remove('productType');
-                  store.set('productType', 'microphone');
-                  redirectToProducts();
-                }}
-              >
-                Microphones
-              </Link>
-            </Card>
-          </Layout.Section> */}
-        </Layout>
-      </Page>
-    );
-  }  
+const Index = () => {
+  return (
+    <Page fullWidth title="Product Search">
+      <Layout>  
+        <ProductTypeCategories productType="audio mixer"/>
+        <ProductTypeCategories productType="headphone"/>
+        <ProductTypeCategories productType="microphone"/>
+      </Layout> 
+    </Page>
+  ); 
 }
 export default Index;
