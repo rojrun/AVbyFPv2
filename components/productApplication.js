8bd9f4487335a8f0cc394ad40cfc8e02c6784687
rojrun <runroj@gmail.com>
2021-02-26 21:19:55 -0800

    put links into const applications

--- components/productApplication.js
+++ components/productApplication.js
@@ -11,26 +11,53 @@ class ProductApplication extends React.Component {
     super(props);
     this.state = {
       app: {},
-      subLinkToShow: false
+      subLinkToShow: [],
+      // links: []
     }
   }
+
+  // static getDerivedStateFromProps(props, state) {
+  //   if (props.links !== state.links) {
+  //     return {links: props.links}
+  //   }
+  //   return null;
+  // }
 
   componentDidMount() {
     this.setState({app: this.context});
   }
 
-  showSubLink = (link, index) => {
-    console.log("link: ", this.props.links[index].subLink);
-    if (!link.subLink) {
-      this.redirectToProductResults(link);
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("should state: ", nextState.links);
+  //   return this.state.links.showLinks !== nextState.links.showLinks;
+  // }
+
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   if (this.state.links.showLinks !== prevState.links.showLinks) {
+  //     return this.state.links.showLinks;
+  //   }
+  //   return null;
+  // }
+
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   if (snapshot !== null) {
+  //     this.setState({links: this.state.links[index].showLinks = true});
+  //   }
+  // }
+
+  showSubLinks = (links, index) => {
+    console.log("Link: ", links);
+    if (!links.subLink) {
+      this.redirectToProductResults(links);
     } else {
-      this.setState({subLinkToShow: true});
-      // this.setState({subLinkToShow: this.props.links[index].subLink});
+      this.setState({subLinkToShow: links.subLink});
+      // this.setState({links: this.state.links[index].showLinks = true});
+      // this.setState({subLinkToShow: true});
     }
   }
 
   redirectToProductResults = (link) => {
-    console.log("redirect link: ", link);
+    // console.log("redirect link: ", link);
     store.remove('tags');
     store.set('tags', link.tags);
     const redirect = Redirect.create(this.state.app);
@@ -47,44 +74,51 @@ class ProductApplication extends React.Component {
 
   render() {
     return (
-      <Layout.Section secondary>
-        <Card sectioned title={this.props.title}>
-          {
-            this.props.links.map((link, index) => {
-              console.log("LInk: ", link);
-              return (
-                <Card.Section key={index}>
-                  <Link
-                    key={index}
-                    onClick={() => {this.showSubLink(link, index)}}
-                  >
-                    {link.topLevel}
-                  </Link>
-                  { 
-                    this.state.subLinkToShow && link.subLink
-                    // this.state.subLinkToShow
-                    ? <Stack distribution="center">
-                        <ButtonGroup>
+      <React.Fragment>
+        {
+          this.props.applications.map((application, index) => {
+            return (
+              <Layout.Section secondary key={index}>
+                <Card sectioned title={Object.keys(application)} key={index}>
+                  {
+                    Object.values(application)[0].map((links, index) => {
+                      console.log("Links: ", links);
+                      return (
+                        <Card.Section key={index}>
+                          <Link
+                            key={index}
+                            onClick={() => {this.showSubLinks(links, index)}}
+                          >
+                            {links.topLevel}
+                          </Link>
                           {
-                            link.subLink.map((link, index) => {  
-                              return (
-                                <Button outline key={index}>
-                                  {link}  
-                                </Button>                        
-                              );
-                            })
+                            this.state.subLinkToShow
+                            ? <Stack distribution="center">
+                                <ButtonGroup>
+                                  {
+                                    this.state.subLinkToShow.map((subLink, index) => {
+                                      return (
+                                        <Button key={index}>
+                                          {subLink}
+                                        </Button>
+                                      )
+                                    })
+                                  }  
+                                </ButtonGroup>  
+                              </Stack>
+                            : null  
                           }
-                        </ButtonGroup>
-                      </Stack>
-                    : null
+                        </Card.Section>
+                      );
+                    })
                   }
-                </Card.Section>
-              );
-            })
-          }
-        </Card>
-      </Layout.Section>
+                </Card>
+              </Layout.Section>        
+            );
+          })
+        }
+      </React.Fragment>
     );
-  }
+  }    
 }
 export default ProductApplication;
--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,99 @@
 import React from 'react';
-import {Layout, Page} from '@shopify/polaris';
+import {Button, ButtonGroup, Card, Layout, Link, Page, Stack} from '@shopify/polaris';
 import ProductApplication from '../components/productApplication.js';
 
 const Index = () => {
+  const applications = [
+    {
+      "Connect ...": [
+        {
+          topLevel: "with others from Home",
+          subLink: ["headphones", "usb microphone"]
+         },
+        {
+          topLevel: "with Huddle Spaces and Conference Rooms",
+          subLink: ["microphone", "processing", "speakers", "video endpoint"]
+         },
+        {
+          topLevel: "Accessories",
+          tags: "accessories"
+        }
+      ]
+    },
+    {
+      "Perform ...": [
+        {
+          topLevel: "Microphones",
+          tags: "microphone"
+        },
+        {
+          topLevel: "Mixers",
+          tags: "mixers"
+        },
+        {
+          topLevel: "PA",
+          tags: "pa"
+        },
+        {
+          topLevel: "Stage Lighting",
+          tags: "stage lighting"
+        },
+        {
+          topLevel: "Accessories",
+          tags: "accessories"
+        }
+      ]
+    },
+    {
+      "Create ...": [
+        {
+          topLevel: "Recorders",
+          tags: "recorders"
+        }
+      ]
+    },
+    {
+      "Present ...": [
+        {
+          topLevel: "with high powered Projectors",
+          tags: "projector"
+        },
+        {
+          topLevel: "your streaming content with A/V Switchers",
+          tags: "av switcher"
+        }
+      ]
+    },
+    {
+      "Entertain ...": [
+        {
+          topLevel: "your events with pro Lighting",
+          subLink: ["centerpiece lighting", "dance floor", "pinspots", "uplights", "washes"]
+        },
+        {
+          topLevel: "your audience with a DJ performance",
+          subLink: ["all-in-one", "controllers"]
+        }
+      ]
+    },
+    {
+      "Deal ...": [
+        {
+          topLevel: "Time Sensitive Specials",
+          tags: ["bundles", "rebates"]
+        },
+        {
+          topLevel: "Open Box Specials",
+          tags: "open box"
+        }
+      ]
+    }
+  ];
+
   return (
     <Page singleColumn title="Let us help You ...">
       <Layout>  
-        <ProductApplication 
-          title="Connect ..."
-          links={[
-            {
-              topLevel: "with others from Home",
-              subLink: ["headphones", "usb microphone"]
-            },
-            {
-              topLevel: "with Huddle Spaces and Conference Rooms",
-              subLink: ["microphone", "processing", "speakers", "video endpoint"]
-            },
-            {
-              topLevel: "Accessories",
-              tags: "accessories"
-            }
-          ]}
-        />
-        <ProductApplication 
-          title="Perform ..."
-          links={[
-            {
-              topLevel: "Microphones",
-              tags: "microphone"
-            },
-            {
-              topLevel: "Mixers",
-              tags: "mixers"
-            },
-            {
-              topLevel: "PA",
-              tags: "pa"
-            },
-            {
-              topLevel: "Stage Lighting",
-              tags: "stage lighting"
-            },
-            {
-              topLevel: "Accessories",
-              tags: "accessories"
-            }
-          ]}
-        />
-        <ProductApplication 
-          title="Create ..."
-          links={[
-            {
-              topLevel: "Recorders",
-              tags: "recorders"
-            }
-          ]}
-        />
-        <ProductApplication 
-          title="Present ..."
-          links={[
-            {
-              topLevel: "with high powered Projectors",
-              tags: "projector"
-            },
-            {
-              topLevel: "your streaming content with A/V Switchers",
-              tags: "av switcher"
-            }
-          ]}
-        />
-        <ProductApplication 
-          title="Entertain ..."
-          links={[
-            {
-              topLevel: "your events with pro Lighting",
-              subLink: ["centerpiece lighting", "dance floor", "pinspots", "uplights", "washes"]
-            },
-            {
-              topLevel: "your audience with a DJ performance",
-              subLink: ["all-in-one", "controllers"]
-            }
-          ]}
-        />
-        <ProductApplication 
-          title="Deals ..."
-          links={[
-            {
-              topLevel: "Time Sensitive Specials",
-              tags: ["bundles", "rebates"]
-            },
-            {
-              topLevel: "Open Box Specials",
-              tags: "open box"
-            }
-          ]}
-        />
+        <ProductApplication applications={applications}/>
       </Layout> 
     </Page>
   ); 
