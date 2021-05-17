6a0658aa8116ead4b4e34999ba0c0dec5537b8b8
rojrun <runroj@gmail.com>
2021-05-17 12:16:58 -0700

    Still working on connecting backend.

--- server.js
+++ server.js
@@ -2,49 +2,85 @@ require('isomorphic-fetch');
 const dotenv = require('dotenv');
 const Koa = require('koa');
 const next = require('next');
-const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
+// const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
+const {shopifyAuth} = require('@shopify/koa-shopify-auth');
 const {verifyRequest} = require('@shopify/koa-shopify-auth');
-const session = require('koa-session');
-dotenv.config();
-const {default: graphQLProxy} = require('@shopify/koa-shopify-graphql-proxy');
-const {ApiVersion} = require('@shopify/koa-shopify-graphql-proxy');
-// const getSubscriptionUrl = require('./server/getSubscriptionUrl');
+const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
+const Router = require('koa-router');
+
+const result = dotenv.config({debug: true});
+if (result.error) {
+  throw result.error;
+}
+console.log("dot result: ", result.parsed); 
+console.log("dot error: ", result.error);
+
+Shopify.Context.initialize({
+  API_KEY: process.env.SHOPIFY_API_KEY,
+  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+  SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
+  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October20,
+  IS_EMBEDDED_APP: true,
+  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+});
+console.log("shopify: ", Shopify.Context);
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
+const app = next({dev: dev});
 const handle = app.getRequestHandler();
-const {SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY} = process.env;
+
+const ACTIVE_SHOPIFY_SHOPS = {};
 
 app.prepare().then(() => {
-    const server = new Koa();
-    server.use(session({secure: true, sameSite: 'none'}, server));
-    server.keys = [SHOPIFY_API_SECRET_KEY];
-    server.use(
-      createShopifyAuth({
-        apiKey: SHOPIFY_API_KEY,
-        secret: SHOPIFY_API_SECRET_KEY,
-        scopes: ['read_products', 'read_customers', 'write_customers', 'read_inventory', 'write_inventory', 'read_orders', 'write_orders', 'write_draft_orders', 'read_draft_orders'],
-        async afterAuth(ctx) {
-          const {shop, accessToken} = ctx.session;
-          ctx.cookies.set('shopOrigin', shop, {
-            httpOnly: false,
-            secure: true,
-            sameSite: 'none'
-          });
-          // await getSubscriptionUrl(ctx, accessToken, shop);
-        },
-      }),
-    );
-    server.use(graphQLProxy({version: ApiVersion.October20}));
-    server.use(verifyRequest());
-    server.use(async (ctx) => {
-      await handle(ctx.req, ctx.res);
-      ctx.respond = false;
-      ctx.res.statusCode = 200;
-      return
-    });
-    server.listen(port, () => {
-      console.log(`> Ready on http://localhost:${port}`);
-    });
+  const server = new Koa();
+  const router = new Router();
+  server.keys = [Shopify.Context.API_SECRET_KEY];
+  console.log("server keys: ", server.keys);
+  server.use(
+    // createShopifyAuth({
+    shopifyAuth({  
+      // afterAuth(ctx) {
+      async afterAuth(ctx) {  
+        const {shop, scope} = ctx.state.shopify;
+        console.log("shop: ", shop);
+        console.log("scope: ", scope);
+        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+        
+        if (ACTIVE_SHOPIFY_SHOPS[shop]) {
+          ctx.redirect(`https://${shop}/admin/apps`);
+        } else {
+          ctx.redirect(`/?shop=${shop}`);
+        }  
+      },
+    }),
+  );
+
+  const handleRequest = async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  };
+  console.log("handleRequest: ", handleRequest);
+  router.get("/", async (ctx) => {
+    const shop = ctx.query.shop;
+
+    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
+      ctx.redirect(`/auth?shop=${shop}`);
+    } else {
+      await handleRequest(ctx);
+    }
+  });
+
+  router.get("(/_next/static/.*)", handleRequest);
+  router.get("/_next/webpack-hmr", handleRequest);
+  router.get("(.*)", verifyRequest(), handleRequest);
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
